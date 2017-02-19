abc3c5029a918fdd5f81badb41b75748eb16c5a8
Anne Hazel <anne.hazel2@gmail.com>
2017-02-19 18:56:51 -0500

    initial setup of index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>A2 Assignment/title>
+
+</head>
+
+<body>
+
+    <h1>Basic Web Application</h1>
+    
+    
+</body>
+</html>
